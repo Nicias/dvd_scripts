991ce426ad7a6767d78b729028fffce72cc31a81
Nicholas Robbins <nickrobbins@yahoo.com>
2014-09-22 22:13:43 -0400

    updates to rip-dvd.sh

--- rip-dvd.sh
+++ rip-dvd.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-#usage rip-dvd.sh [dev [title1[,title2[,title3...]]]]
+#usage rip-dvd.sh [dev [mode|title1[,title2[,title3...]]]]
 
 /usr/bin/ionice -c3 -p$$
 
 USERGROUP="me:mine"
-DESTBASE=/var/srv/media/videos/NO-BACKUP-holding/ripping/
+
+HOME=/root
+LOGNAME=root
+
+cd /root
+. /etc/profile
 
 if [ -z "$1" ]; then 
 	TARGET=/dev/sr0
@@ -33,7 +38,7 @@ if [ -z "$2" ]; then
 		/usr/bin/time -v /opt/bin/makemkvcon --profile="/root/.MakeMKV/default.mmcp.xml" mkv --minlength=3600 -r --decrypt --directio=true dev:${TARGET} all ${DESTDIR}
 	else
 		/bin/echo "episodes present, ripping all"
-		/usr/bin/time -v /opt/bin/makemkvcon --profile="/root/.MakeMKV/default.mmcp.xml" mkv --minlength=300 -r --decrypt --directio=true dev:${TARGET} all ${DESTDIR}
+		/usr/bin/time -v /opt/bin/makemkvcon --profile="/root/.MakeMKV/default.mmcp.xml" mkv --minlength=120 -r --decrypt --directio=true dev:${TARGET} all ${DESTDIR}
 		/usr/bin/find  ${DESTDIR} -size +4G -delete
 	fi
 else
@@ -53,6 +58,18 @@ else
 		done
 	fi
 fi
+
+cd ${DESTDIR}
+
+for VIDEO in *.mkv
+	do
+	/bin/echo ${VIDEO} >> ${REPORT}
+	PROBE=`/usr/bin/ffprobe -select_streams v -show_streams -count_frames -i ${VIDEO} 2>&1`
+	/bin/echo "$PROBE" | /bin/grep 'Duration\|Stream\|read_frames' >> ${REPORT}
+	DSTRING=`/bin/echo "$PROBE" | /bin/grep Duration | /usr/bin/cut -d" " -f4 | /bin/sed -e "s/,//" -e "s/:/)*60+/g"`
+	FC=`/bin/echo "$PROBE" | /bin/grep nb_read_frames | /usr/bin/cut -d"=" -f2`
+	/bin/echo "fps: "`/bin/echo $FC"/((("$DSTRING")" | /usr/bin/bc -l` >> ${REPORT}
+done
 
 /bin/chown -R ${USERGROUP} ${DESTDIR}
 if [ -z "$2" ]; then 
